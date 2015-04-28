4a483d6ee8fec1202977a710c45d10f9927ed703
velo <iMac@velo-iMac.local>
2015-04-28 09:54:08 +0200

    Initial Commit

---
+++ Lafosca/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lafosca
+//
+//  Created by velo on 28/04/15.
+//  Copyright (c) 2015 veloapps.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lafosca/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lafosca
+//
+//  Created by velo on 28/04/15.
+//  Copyright (c) 2015 veloapps.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
